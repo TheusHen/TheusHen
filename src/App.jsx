7d869465a46a957af75dd0b58e465408883b54f6
I'm Matheus <codelong@proton.me>
2024-11-01 15:12:37 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ const App = () => {
   return (
     <Router>
       <Routes>
+      <SpeedInsights/>
         <Route path="/" element={<Boot />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/about" element={<About />} />
@@ -22,7 +23,6 @@ const App = () => {
         <Route path="*" element={<NotFound />} />
       </Routes>
       <Analytics />
-      <SpeedInsights/>
     </Router>
   );
 };
