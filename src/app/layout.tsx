e7043117728f3c3cb6d821bf505aec3339fe2f53
I'm Matheus <codelong@proton.me>
2025-05-24 12:12:19 -0300

    Implement Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -63,14 +63,14 @@ export default function RootLayout({
             {/* Google Tag Manager */}
             <Script
                 async
-                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GTAG_ID}`}
+                src={`https://www.googletagmanager.com/gtag/js?id=G-DWX5JVERXC`}
             />
             <Script id="google-analytics" strategy="afterInteractive">
                 {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GTAG_ID}');
+            gtag('config', 'G-DWX5JVERXC');
           `}
             </Script>
         </head>
