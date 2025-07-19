553d19fb36666c21ff3e86438ab776c11c1c10e8
I'm Matheus <codelong@proton.me>
2025-07-19 20:47:40 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -90,8 +90,8 @@ const geistMono = Geist_Mono({
 });
 
 export default function RootLayout({
-                                       children,
-                                   }: Readonly<{
+    children,
+}: Readonly<{
     children: React.ReactNode;
 }>) {
     return (
@@ -106,11 +106,11 @@ export default function RootLayout({
             <Script async src="https://www.googletagmanager.com/gtag/js?id=G-DWX5JVERXC" />
             <Script id="google-analytics" strategy="afterInteractive">
                 {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-DWX5JVERXC');
-          `}
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-DWX5JVERXC');
+                `}
             </Script>
             
             {/* Ahrefs Analytics */}
@@ -124,10 +124,24 @@ export default function RootLayout({
             <Script defer data-domain="theushen.me" src="https://plausible.io/js/script.js" />
             <Script id="plausible-init" strategy="afterInteractive">
                 {`
-            window.plausible = window.plausible || function() {
-              (window.plausible.q = window.plausible.q || []).push(arguments)
-            }
-          `}
+                    window.plausible = window.plausible || function() {
+                        (window.plausible.q = window.plausible.q || []).push(arguments)
+                    }
+                `}
+            </Script>
+
+            {/* Hotjar Analytics */}
+            <Script id="hotjar-init" strategy="afterInteractive">
+                {`
+                    (function(h,o,t,j,a,r){
+                        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                        h._hjSettings={hjid:6469301,hjsv:6};
+                        a=o.getElementsByTagName('head')[0];
+                        r=o.createElement('script');r.async=1;
+                        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                        a.appendChild(r);
+                    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+                `}
             </Script>
 
             <Script id="structured-data" type="application/ld+json">
